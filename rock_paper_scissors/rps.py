58ccec65f548b1b4f4eaacd9c71a2028e79ab026
maxgunter99 <maxgunter99@gmail.com>
2019-10-10 16:39:11 -0500

    added rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,56 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+
+  rps = [ 'rock' , 'paper' , 'scissors' ]
+
+  answer = []
+
+  # maybe try recursion?
+
+  if n == 0:
+    return [[]]
+
+  elif n == 1:
+
+    for i in range( len( rps ) ):
+      solution = [ rps[i] ]
+      answer.append( solution )
+
+  elif n == 2:
+
+    for i in range( len( rps ) ):
+      for x in range( len( rps ) ):
+        answer.append( [rps[i] , rps[x]] )
+  elif n == 3:
+
+    for i in range( len( rps ) ):
+      for x in range( len( rps ) ):
+        for y in range( len( rps ) ):
+          answer.append( [rps[i] , rps[x] , rps[y]] )
+
+  elif n == 4:
+    for i in range( len( rps ) ):
+      for x in range( len( rps ) ):
+        for y in range( len( rps ) ):
+          for z in range( len( rps ) ):
+            answer.append( [rps[i] , rps[x] , rps[y] , rps[z]] )
+
+  elif n == 5:
+    for i in range( len( rps ) ):
+      for x in range( len( rps ) ):
+        for y in range( len( rps ) ):
+          for z in range( len( rps ) ):
+            for u in range( len( rps ) ):
+              answer.append( [rps[i] , rps[x] , rps[y] , rps[z] , rps[u]] )
+
+  return answer
+      
+
+  
+
+rock_paper_scissors(1)
+# [['rock', 'rock'], ['rock', 'paper'], ['rock', 'scissors'], ['paper', 'rock'], ['paper', 'paper'], ['paper', 'scissors'], ['scissors', 'rock'], ['scissors', 'paper'], ['scissors', 'scissors']]
 
 
 if __name__ == "__main__":
